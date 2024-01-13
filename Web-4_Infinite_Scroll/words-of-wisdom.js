778825121c93b2e7f7926b863eb59749272ee498
Shakir <shakirali.sacube@gmail.com>
2024-01-13 16:30:46 +0530

    Added Infinite Scroll Challenge

---
+++ Web-4_Infinite_Scroll/words-of-wisdom.js
@@ -0,0 +1,26 @@
+export const wordsOfWisdom = `
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+`.split(" ");
+
+import figlet from 'https://cdn.jsdelivr.net/npm/figlet@1.7.0/+esm'
+
+// Example using asynchronous fetch without explicit error handling
+fetch('http://127.0.0.1:5500/fonts/Standard.flf')
+    .then(response => response.text())
+    .then(fontData => {
+        figlet.parseFont('Standard', fontData);
+    });
+
+
+export function generateWisdom(wordIndex = 0) {
+    // Reset word index to 0 when all words have been used
+    if (wordIndex == wordsOfWisdom.length) wordIndex = 0;
+
+    const wisdom = figlet.textSync(wordsOfWisdom[wordIndex]);
+    return [wordIndex + 1, wisdom];
+}
