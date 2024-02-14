b0d6896bfad8955a2cc8a9f31849318459573535
redtrib3 <anirudhkp2004@gmail.com>
2024-02-14 19:46:02 +0530

    Changes: * Updated flag. * Made secretkey env var.

--- Web-3_SSTI/app/config.py
+++ Web-3_SSTI/app/config.py
@@ -7,12 +7,12 @@ DB_PATH = os.path.join(curr_dir, 'db/auth.db')
 
 
 class Config:
-    SECRET_KEY = '8a7b#Bc1$%7dEf2Gh3*Jk1lMn0Pq4rSt5Uv6WxYz'
-    #SECRET_KEY = os.getenv("SECRET_KEY")
+    #SECRET_KEY = '8a7b#Bc1$%7dEf2Gh3*Jk1lMn0Pq4rSt5Uv6WxYz'
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
+
 #	 TODO: try filesystem sessions if session concurrency issues occur
 #    SESSION_TYPE = 'filesystem'
 #    SESSION_FILE_DIR = 'sessions/'
